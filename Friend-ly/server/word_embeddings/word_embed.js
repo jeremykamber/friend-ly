5b0ac84e4625f8e8329341297f188e1ec866875f
rushil-arun <rushilarun@gmail.com>
2025-02-05 10:03:23 -0800

    Tested splitting interests into individual words and comparing them one by one

--- Friend-ly/server/word_embeddings/word_embed.js
+++ Friend-ly/server/word_embeddings/word_embed.js
@@ -1,4 +1,4 @@
-const tf_backend = require('@tensorflow/tfjs-node')
+const tf_backend = require('@tensorflow/tfjs-node')/*
 const tensorflow = require("@tensorflow-models/universal-sentence-encoder")
 
 function cosineComparison(vector1, vector2) {
@@ -16,7 +16,71 @@ async function compareSentences(text1, text2) {
     return Math.round(cosineComparison(vector1, vector2) * 100) + "%"
 }
 
-const sentence1 = "european football"
-const sentence2 = "european football"
+//const sentence1 = "european football"
+//const sentence2 = "european football"
+const sentence1 = ["football", "parks", "swimming", "motorcycles"]
+const sentence2 = ["camping", "soccer", "freestyle", "bikes"]
 
-compareSentences(sentence1, sentence2).then(console.log)
+async function compare() {
+    for (let i = 0; i < sentence1.length; i++) {
+        for (let j = 0; j < sentence2.length; j++) {
+            console.log(await compareSentences(sentence1[i], sentence2[j]))
+            //console.log(sentence1[i] + ", " + sentence2[j] + ": ")
+        }
+    }
+}
+compare()
+
+//compareSentences(sentence1[0], sentence2[1]).then(console.log)
+
+//compareSentences(sentence1, sentence2).then(console.log)*/
+const tensorflow = require("@tensorflow-models/universal-sentence-encoder");
+
+let model;
+
+async function loadModel() {
+    if (!model) {
+        model = await tensorflow.load();
+        console.log("Model loaded!");
+    }
+}
+
+//loadModel();
+
+function cosineComparison(vector1, vector2) {
+    const dotProduct = vector1.reduce((sum, val, i) => sum + val * vector2[i], 0);
+    const magnitude1 = Math.sqrt(vector1.reduce((sum, val) => sum + val ** 2, 0));
+    const magnitude2 = Math.sqrt(vector2.reduce((sum, val) => sum + val ** 2, 0));
+    return dotProduct / (magnitude1 * magnitude2);
+}
+
+async function compareSentences(text1, text2) {
+    //await loadModel();
+    const embeddings = await model.embed([text1, text2]);
+
+    try {
+        const vectors = embeddings.dataSync(); // Faster than .array()
+        return Math.round(cosineComparison(vectors.slice(0, 512), vectors.slice(512, 1024)) * 100) + "%";
+    } finally {
+        embeddings.dispose(); // Prevent memory leaks
+    }
+}
+
+const sentence1 = ["", "lakers", "biking", "road tripping"];
+const sentence2 = ["cycling", "luka doncic", "freestyle", "travel"];
+
+async function runComparisons() {
+    await loadModel(); // Load model once
+
+    const tasks = [];
+    for (let i = 0; i < sentence1.length; i++) {
+        for (let j = 0; j < sentence2.length; j++) {
+            tasks.push(compareSentences(sentence1[i], sentence2[j]).then(result => {
+                console.log(`${sentence1[i]}, ${sentence2[j]}: ${result}`);
+            }));
+        }
+    }
+    await Promise.all(tasks); // Run comparisons in parallel
+}
+
+runComparisons();
