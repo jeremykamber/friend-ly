5d8ac4bb50eb974f9a14f110d68fa1c19f135e34
Slys <slysdeveloper@gmail.com>
2025-02-27 19:51:11 -0800

    fix(settings): remove leftover nonexistant edit email route from settings navigator

--- Friend-ly/ui/navigation/SettingsNavigator.jsx
+++ Friend-ly/ui/navigation/SettingsNavigator.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import SettingsView from "../screens/SettingsView";
 import EditUsernameView from "../screens/EditUsernameView";
-import EditEmailView from "../screens/EditEmailView";
 
 const Stack = createStackNavigator();
 
@@ -19,14 +18,6 @@ function SettingsNavigator() {
         component={EditUsernameView}
         options={{ 
           title: "Edit Username",
-          headerShown: true,
-        }}
-      />
-      <Stack.Screen 
-        name="EditEmail" 
-        component={EditEmailView}
-        options={{ 
-          title: "Edit Email",
           headerShown: true,
         }}
       />
