491d1d4e6d0ccc16902ebb419ebd541353ba2a3e
Slys <slysdeveloper@gmail.com>
2025-03-03 23:26:54 -0800

    feat(navigation): add ChatsNavigator and update TabNavigator to use it

--- Friend-ly/ui/navigation/TabNavigator.jsx
+++ Friend-ly/ui/navigation/TabNavigator.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import LoginView from "../screens/LoginView";
-import ChatView from "../screens/ChatView";
+import ChatView from "../screens/ChatsView";
 import SettingsNavigator from "./SettingsNavigator";
 import RecommendationView from "../screens/RecommendationView";
+import ChatsNavigator from "./ChatsNavigator";
 
 const Tab = createBottomTabNavigator();
 
@@ -23,7 +24,7 @@ function TabNavigator() {
             />
             <Tab.Screen
                 name="Chat"
-                component={ChatView}
+                component={ChatsNavigator}
                 options={{
                     tabBarIcon: ({ color, size }) => (
                         <Ionicons name="chatbubble" size={size} color={color} />
